c56feb036d98205006149c036e025ea007e5a986
AaronWard <aaronlaptop12@hotmail.com>
2018-04-24 17:31:38 +0100

    added cors 3

--- app.py
+++ app.py
@@ -102,5 +102,5 @@ if __name__ == "__main__":
     print(("* Loading model and starting Flask server..."
         "please wait until server has fully started"))
     app.debug = True
-    # load_model(MODEL_PATH)
-    app.run(host='0.0.0.0')
+    port = int(os.environ.get("PORT", 6000))
+    app.run(host='0.0.0.0', port=port)
